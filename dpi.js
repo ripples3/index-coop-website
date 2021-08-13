045e7591f8804d18fc00cf80ef51e94198cc25ae
Don Bustos <don_daryl_20@yahoo.com>
2021-08-13 20:55:41 +0800

    updated dpi prices

--- dpi.js
+++ dpi.js
@@ -3,13 +3,20 @@ $.getJSON(
     function(data) {
         console.log(data);
 
-        var currentprice = data.market_data.current_price.usd;
+        var currentprice = "$"+data.market_data.current_price.usd;
+        var pricechange = data.market_data.price_change_percentage_24h+"%";
+        var marketcap = "$"+data.market_data.market_cap.usd;
+        var volume = data.market_data.total_volume.usd;
+        var currentsupply = data.market_data.circulating_supply.usd;
 
 
 
-        $(".currentprice").append(currentprice)
-      
-    
+        $(".currentprice").append(currentprice);
+        $(".pricechange").append(pricechange);
+        $(".marketcap").append(marketcap);
+        $(".volume").append(volume);
+        $(".currentsupply").append(currentsupply);
+
         
 
     }   
