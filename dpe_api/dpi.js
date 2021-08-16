12dedebe522c3ea53ee81e787c1d75b83564e823
Don Bustos <don_daryl_20@yahoo.com>
2021-08-17 05:49:50 +0800

    updated DPI price change

--- dpe_api/dpi.js
+++ dpe_api/dpi.js
@@ -3,7 +3,7 @@ fetch('https://api.coingecko.com/api/v3/coins/defipulse-index')
 .then(data => {
   console.log(data) // Prints result from `response.json()` in getRequest
   var currentpricenow = "$"+data.market_data.current_price.usd;
-  var pricechange = data.market_data.price_change_percentage_24h.toFixed(2)+"%";
+  var pricechange = data.market_data.price_change_percentage_24h;
   var marketcap = "$"+ Math.fround(data.market_data.market_cap.usd/1000000).toFixed(2)+"M";
   var volume = Math.fround(data.market_data.total_volume.usd/1000000).toFixed(2)+"M";
   var currentsupply = Math.floor(data.market_data.circulating_supply).toLocaleString('en');
@@ -11,10 +11,19 @@ fetch('https://api.coingecko.com/api/v3/coins/defipulse-index')
 
 
   $(".currentpricenow").append(currentpricenow);
-  $(".pricechange").append(pricechange);
+ // $(".pricechange").append(pricechange);
   $(".marketcap").append(marketcap);
   $(".volume").append(volume);
   $(".currentsupply").append(currentsupply);
+
+  if (pricechange >= 0) {
+    $(".pricechange").append((pricechange).toFixed(2)+"%");
+    $(".pricechange").css({'color':'green'});
+ }
+ else {
+    $(".pricechange").append((pricechange).toFixed(2)+"%");
+    $(".pricechange").css({'color':'red'});
+ }
 
 
 
